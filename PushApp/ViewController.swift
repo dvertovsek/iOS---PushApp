f790988ff7bbda0e4441142c10a0d1e00d4293dd
Darijan <dvertovs@foi.hr>
2016-01-19 00:04:45 +0100

    login screen completed

--- PushApp/ViewController.swift
+++ PushApp/ViewController.swift
@@ -7,29 +7,74 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        label.alpha = 0
-        label.setTextWithTypeAnimation("Check events", characterInterval: 0.08)
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)) {
-            UIView.animateWithDuration(1.9) { () -> Void in
-                self.label.alpha = 1
-                self.label.alpha = 1
-            }
+        let image: UIImage = UIImage(named: "nature")!
+        self.view.alpha = 0.2
+        self.view.backgroundColor = UIColor(patternImage: image)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let view = UIView()
+        view.backgroundColor = UIColor.clearColor()
+        view.heightAnchor.constraintEqualToConstant(self.view.frame.size.height * 0.4).active = true
+        stackView.addArrangedSubview(view)
+        
+        let usernameTxtField = UITextField()
+//        usernameTxtField.placeholder = "username"
+        usernameTxtField.backgroundColor = UIColor(red: 194/255, green: 79/255, blue: 209/255, alpha: 0.6)
+//        usernameTxtField.textColor = UIColor.greenColor()
+        
+        let attr = [NSForegroundColorAttributeName:UIColor.blackColor()]
+        usernameTxtField.attributedPlaceholder = NSAttributedString.init(string: "user", attributes: attr)
+        usernameTxtField.textAlignment = NSTextAlignment.Center
+        usernameTxtField.heightAnchor.constraintEqualToConstant(self.view.frame.size.height * 0.1).active = true
+        
+//        let bottomBorder = CALayer()
+//        bottomBorder.frame = CGRectMake(0.0, usernameTxtField.frame.size.height - 1, usernameTxtField.frame.size.width, 1.0);
+//        bottomBorder.backgroundColor = UIColor.blackColor().CGColor
+//        usernameTxtField.layer.addSublayer(bottomBorder)
+        
+        stackView.addArrangedSubview(usernameTxtField)
+        
+        
+        let passTxtField = UITextField()
+//        passTxtField.placeholder = "*******"
+        passTxtField.secureTextEntry = true
+        passTxtField.backgroundColor = UIColor(red: 194/255, green: 79/255, blue: 209/255, alpha: 0.6)
+        
+        passTxtField.attributedPlaceholder = NSAttributedString.init(string: "*******", attributes: attr)
+        passTxtField.textAlignment = NSTextAlignment.Center
+        passTxtField.heightAnchor.constraintEqualToConstant(self.view.frame.size.height * 0.1).active = true
+        
+//        let bottomBorder = CALayer()
+//        bottomBorder.frame = CGRectMake(0.0, passTxtField.frame.size.height - 1, passTxtField.frame.size.width, 1.0);
+//        bottomBorder.backgroundColor = UIColor.blackColor().CGColor
+//        passTxtField.layer.addSublayer(bottomBorder)
+        
+        stackView.addArrangedSubview(passTxtField)
+        
+        let view2 = UIView()
+        view2.backgroundColor = UIColor.clearColor()
+        view2.heightAnchor.constraintEqualToConstant(self.view.frame.size.height * 0.4).active = true
+        stackView.addArrangedSubview(view2)
+        
+        UIView.animateWithDuration(5) { () -> Void in
+            self.view.alpha = 1.0
         }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
