e61fda8aa74901435203b4d08632ec00e04dc34e
Darijan <dvertovs@foi.hr>
2016-01-18 16:09:25 +0100

    Initial Commit

---
+++ PushApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushApp
+//
+//  Created by MTLab on 18/01/16.
+//  Copyright © 2016 dare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
